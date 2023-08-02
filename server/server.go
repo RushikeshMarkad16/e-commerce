f7e465e37364f3b38627d1880d9a05373499fc06
Rushikesh Markad <rushikesh.markad@joshsoftware.com>
2023-08-02 14:40:52 +0530

    Initialized the grpc server

--- server/server.go
+++ server/server.go
@@ -2,13 +2,21 @@ package server
 
 import (
 	"fmt"
+	"log"
+	"net"
 	"strconv"
+	"sync"
 
 	"github.com/RushikeshMarkad16/e-commerce/config"
+	"github.com/RushikeshMarkad16/e-commerce/utils/productpb"
 	"github.com/urfave/negroni"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
-func StartAPIServer() {
+func StartAPIServer(wg *sync.WaitGroup) {
+	fmt.Println("Starting Rest Server....")
+	defer wg.Done()
 	port := config.AppPort()
 	server := negroni.Classic()
 
@@ -21,4 +29,26 @@ func StartAPIServer() {
 	server.UseHandler(router)
 	addr := fmt.Sprintf(":%s", strconv.Itoa(port))
 	server.Run(addr)
+}
+
+func StartgRPCServer(wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Starting grpc server....")
+	listen, err := net.Listen("tcp", ":50051")
+
+	if err != nil {
+		log.Fatalf("Failed to listen : %v", err)
+	}
+
+	dependencies, err := initDependencies()
+	if err != nil {
+		panic(err)
+	}
+	s := grpc.NewServer()
+
+	productpb.RegisterProductServiceServer(s, NewGrpcServer(dependencies.ProductService))
+	reflection.Register(s)
+	if err := s.Serve(listen); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
