c8591031f7b308e51054c807fff73b86125df59e
Rushikesh Markad <rushikesh.markad@joshsoftware.com>
2023-08-17 14:50:25 +0530

    Removed code for ping handler

--- server/router.go
+++ server/router.go
@@ -3,7 +3,6 @@ package server
 import (
 	"net/http"
 
-	"github.com/RushikeshMarkad16/e-commerce/api"
 	"github.com/RushikeshMarkad16/e-commerce/order"
 	"github.com/RushikeshMarkad16/e-commerce/product"
 	"github.com/gorilla/mux"
@@ -14,7 +13,6 @@ func initRouter(dep dependencies) (router *mux.Router) {
 	router = mux.NewRouter()
 
 	//Product
-	router.HandleFunc("/ping", pingHandler).Methods(http.MethodGet)
 	router.HandleFunc("/products", product.List(dep.ProductService)).Methods(http.MethodGet)
 	router.HandleFunc("/products/{id}", product.FindByID(dep.ProductService)).Methods(http.MethodGet)
 
@@ -25,8 +23,4 @@ func initRouter(dep dependencies) (router *mux.Router) {
 	router.HandleFunc("/order/status", order.UpdateStatus(dep.OrderService)).Methods(http.MethodPatch)
 
 	return
-}
-
-func pingHandler(rw http.ResponseWriter, req *http.Request) {
-	api.Success(rw, http.StatusOK, api.Response{Message: "pong"})
 }
